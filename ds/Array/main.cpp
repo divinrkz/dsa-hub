3581bc4ed371b9c8ec965aaf44534d2324d584cb
Divin Irakiza <irakizadivin@gmail.com>
2021-05-01 21:25:09 +0200

    Array operations testing @v1

--- ds/Array/main.cpp
+++ ds/Array/main.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 int main () {
 
-    int array1[] = {1, 2, 3, 4};
+    int array[] = {1, 2, 3, 4};
 
-     print("Array elements", array1, 5);
+     print("Array elements", array, 5);
     //  showArrayContiguity("Array memory locations", array1, 5);
     //  insertAtBeginning(array1, 5, 4, 0);
     //  insertAtAGivenIndex(array1, 5, 4, 4, 5);
     //  insertBeforeAGivenIndex(array1, 5, 4, 1, 5);
-    insertX(array1, 4, 5, 2);
+    // insertX(array1, 4, 5, 2);
 
     return 0;
 }
