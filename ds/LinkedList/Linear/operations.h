eee81e651bca832e402b882291cc35e63f1c1195
Divin Irakiza <irakizadivin@gmail.com>
2021-05-02 19:56:38 +0200

    LinkedList InsertMany function

--- ds/LinkedList/Linear/operations.h
+++ ds/LinkedList/Linear/operations.h
@@ -20,7 +20,7 @@ void print(string title, Node* head) {
 
     while (node != NULL) {
         cout << " " << node->data <<  " ";
-        node = ptr->next;
+        node = node->next;
     }
 
     cout << "]" << endl;
@@ -28,12 +28,51 @@ void print(string title, Node* head) {
 
 
 
-Node* insert(Node* head, int data) {
+Node* insert(Node* head, int data, string position="END") {
     Node* node = new Node;
     node->data = data;
-    node->next = NULL;
 
-    head->next = node;
+    if (position == "END") {
+        node->next = NULL;
+        head->next = node;
+        print("LinkedList Elements After Insertion at END", head);
+    }
+    else if (position == "START") {
+        node->next = head;
+    } 
     return head;
 }
 
+
+
+Node* insertHeadNode(Node* head, int data) {
+    Node *node = new Node;
+    node->data = data;
+    node->next = head;
+    head = node;
+
+    print("LinkedList Elements After Insertion At HEAD", head);
+
+    return head;
+
+}
+
+
+Node* insertMany(Node* head, int array[], int len){
+    Node* last = head;
+    Node* node;
+    
+
+    for (int i = 0; i < len; i++) {
+        node = new Node;
+        node->data = array[i];
+        node->next = NULL;
+        last->next = node;
+        last = node;
+    }
+
+    print("LinkedList Elements After Insertion Many", head);
+
+
+    return head;
+}
