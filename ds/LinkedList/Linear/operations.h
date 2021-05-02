7488e0c5469365ceef84ab632b57278351cee217
Divin Irakiza <irakizadivin@gmail.com>
2021-05-02 20:16:43 +0200

    LinkedInsertion at any position implementation

--- ds/LinkedList/Linear/operations.h
+++ ds/LinkedList/Linear/operations.h
@@ -5,10 +5,27 @@
 using namespace std;
 
 
-Node* create(int data){
+Node* getNode(int data) {
     Node* node = new Node;
     node->data = data;
     node->next = NULL;
+    
+    return node;
+}
+
+Node* search(Node *head, int data) {
+    Node *node = head;
+    while(p!=NULL) {
+            if (key == p->flightStop.name)
+                return p;
+        p = p->next;
+        }
+        return NULL;
+}
+
+Node* create(int data){
+    Node* node = getNode(data);
+    return node;
 }
 
 
@@ -29,8 +46,7 @@ void print(string title, Node* head) {
 
 
 Node* insert(Node* head, int data, string position="END") {
-    Node* node = new Node;
-    node->data = data;
+    Node* node = getNode(data);
 
     if (position == "END") {
         node->next = NULL;
@@ -42,22 +58,10 @@ Node* insert(Node* head, int data, string position="END") {
         head = node;
         print("LinkedList Elements After Insertion At HEAD", head);
     } 
+
     return head;
 }
 
-
-
-Node* insertHeadNode(Node* head, int data) {
-    Node *node = new Node;
-    node->data = data;
-    node->next = head;
-    head = node;
-
-    print("LinkedList Elements After Insertion At HEAD", head);
-
-    return head;
-
-}
 
 
 Node* insertMany(Node* head, int array[], int len){
@@ -66,14 +70,51 @@ Node* insertMany(Node* head, int array[], int len){
     
 
     for (int i = 0; i < len; i++) {
-        node = new Node;
-        node->data = array[i];
-        node->next = NULL;
+        node = getNode(array[i]);
         last->next = node;
         last = node;
     }
 
     print("LinkedList Elements After Insertion Many", head);
+
+
+    return head;
+}
+
+
+
+Node* replaceAt(Node* head, int search, int data) {
+    Node* temp = head;
+
+    while (temp->data != search) {
+        temp = temp->next;
+    }
+
+    temp->data = data;
+
+    print("LinkedList Elements After Replacing", head);
+
+
+    return head;
+}
+
+
+Node* insertBefore(Node* head, int search, int data) {
+    Node* temp = head;
+    Node* previous;
+
+    while (temp->data != search) {
+        previous = temp;
+        temp = temp->next;
+    }
+
+    Node* node = getNode(data);
+
+    previous->next = node;
+    node->next = temp;
+
+
+    print("LinkedList Elements After Inserting before", head);
 
 
     return head;
